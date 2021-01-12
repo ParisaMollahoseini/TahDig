29bf9eb9c3c49396bbd6d040ef46188f4323987a
parisa1377 <mollahoseini77@gmail.com>
2021-01-12 17:12:37 +0330

    add login button

--- app/src/main/java/com/example/tahdig/MainActivity.kt
+++ app/src/main/java/com/example/tahdig/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tahdig
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent1 = Intent(this,login_signup::class.java)
+        var intent1_3 = intent
     }
 }
--- app/src/main/java/com/example/tahdig/login_signup.kt
+++ app/src/main/java/com/example/tahdig/login_signup.kt
@@ -1,5 +1,6 @@
 package com.example.tahdig
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class login_signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_signup)
+        val intent1 = Intent(this,MainActivity::class.java)
+        var intent1_3 = intent
     }
 }
