9b0974a6a2e07864d855f0746cd17aa8578496e9
parisa1377 <mollahoseini77@gmail.com>
2021-01-12 17:55:51 +0330

    start page

--- app/src/main/java/com/example/tahdig/MainActivity.kt
+++ app/src/main/java/com/example/tahdig/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.tahdig
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val intent1 = Intent(this,login_signup::class.java)
-        var intent1_3 = intent
+
+
+        start_button.setOnClickListener {
+            startActivity(intent1)
+        }
     }
 }
