445e9453dd465aaa11711caba67089401f83730b
parisa1377 <mollahoseini77@gmail.com>
2021-01-12 20:11:39 +0330

    signUp

--- app/src/main/java/com/example/tahdig/signup.kt
+++ app/src/main/java/com/example/tahdig/signup.kt
@@ -2,10 +2,15 @@ package com.example.tahdig
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_signup.*
 
 class signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        accept_signup_button.setOnClickListener {
+
+        }
     }
 }
