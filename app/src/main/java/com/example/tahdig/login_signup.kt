09030d1e8c8a757a33cbbaef19ce0e1f37526453
parisa1377 <mollahoseini77@gmail.com>
2021-01-12 19:20:11 +0330

    start page

--- app/src/main/java/com/example/tahdig/login_signup.kt
+++ app/src/main/java/com/example/tahdig/login_signup.kt
@@ -3,12 +3,13 @@ package com.example.tahdig
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login_signup.*
 
 class login_signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_signup)
-        val intent1 = Intent(this,MainActivity::class.java)
-        var intent1_3 = intent
+
+
     }
 }
