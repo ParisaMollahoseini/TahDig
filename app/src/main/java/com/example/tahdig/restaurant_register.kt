2a595aeaa1ef74c4d42fc5a9d13996c851a18b2a
parisa1377 <mollahoseini77@gmail.com>
2021-02-03 13:52:22 +0330

    restaurant_register.kt

---
+++ app/src/main/java/com/example/tahdig/restaurant_register.kt
@@ -0,0 +1,11 @@
+package com.example.tahdig
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class restaurant_register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_register)
+    }
+}
