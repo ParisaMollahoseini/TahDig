2d5bd8fb1c639b9db3ee6ef3dbeb7a1df8659dbc
khoramian <khoramian8@gmail.com>
2021-02-03 05:48:47 -0700

    sd

--- app/src/main/java/com/example/tahdig/restaurant_register.kt
+++ app/src/main/java/com/example/tahdig/restaurant_register.kt
@@ -54,10 +54,10 @@ class restaurant_register : AppCompatActivity() {
                 if (res_addr != -1.toLong())
                 {
                     val query = "Select * from Loggedperson"
-//                    val result = db.rawQuery(query, null)
-//                    result.moveToFirst()
-//                    val ownerUsername = result.getString(result.getColumnIndex("username"))
-//                    db.insertNewRequests(resName,ownerUsername,resbusinessLicenseNumber,resPhone,res_addr.toInt())
+                    val result = db.readableDatabase.rawQuery(query, null)
+                    result.moveToFirst()
+                    val ownerUsername = result.getString(result.getColumnIndex("username"))
+                    db.insertNewRequests(resName,ownerUsername,resbusinessLicenseNumber,resPhone,res_addr.toInt())
                 }
 
                 /// add registration request
